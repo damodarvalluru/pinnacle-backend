3804754c0dea107075d2103b61d64634049e2589
DAMODAR VALLURU <damodarvalluru7@gmail.com>
2026-05-27 14:32:30 +0530

    Production frontend connection completed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 
 // 2. Enable CORS globally before setting up route structures
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500','https://damodarvalluru.github.io'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500','https://damodarvalluru.github.io/pinnacle-scholars'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
