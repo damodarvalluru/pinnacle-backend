5cbd51899c9a131535de96bdfaebefe3362df0a6
DAMODAR VALLURU <damodarvalluru7@gmail.com>
2026-05-27 12:12:33 +0530

    some updations are done by railway db

--- db.js
+++ db.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'pinnacle_academy',
+
+    host: process.env.DB_HOST,
+
+    user: process.env.DB_USER,
+
+    password: process.env.DB_PASSWORD,
+
+    database: process.env.DB_NAME,
+
+    port: process.env.DB_PORT,
+
     waitForConnections: true,
-    connectionLimit: 10
+
+    connectionLimit: 10,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
+
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 
 // 2. Enable CORS globally before setting up route structures
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500','https://damodarvalluru.github.io'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
